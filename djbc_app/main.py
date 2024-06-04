4b9b158d240f6cebe05d24982b935b584fbb5d83
Fauzan Andri <fauzanandri11@gmail.com>
2024-06-04 14:09:24 +0700

    add cors

--- djbc_app/main.py
+++ djbc_app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models, service_clients, schemas
 from .database import SessionLocal, engine
@@ -8,6 +9,16 @@ from .database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency
 def get_db():
@@ -42,3 +53,7 @@ def create_billing(billing: schemas.BillingCreate, db: Session = Depends(get_db)
 # if db_user:
 #     raise HTTPException(status_code=400, detail="Email already registered")
     return service_clients.create_billing(db=db, billing=billing)
+
+@app.post("/pay-billing")
+def pay_billing(, db: Session = Depends(get_db)):
+    
